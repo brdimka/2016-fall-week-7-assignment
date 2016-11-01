460258c72478157c98cca5f35d6dc58534694af4
brdimka <briandimka@Brians-MacBook-Pro-2.local>
2016-11-01 12:53:19 -0400

    updare

--- 7.1/script.js
+++ 7.1/script.js
@@ -16,22 +16,29 @@ var plot = d3.select('.canvas')
 
 var scaleX, scaleY;
 
-//Step 1: importing multiple datasets
-d3.queue()
-    //.defer()
-    //.defer()
-    //.defer()
+//Step 1: importing multiple datasets - Each defer command has 3 arguements
+//it doesn't need to be exactly 3 argumentts.
+d3.queue() //HAS TO BE IN CORRECT ORDER!
+    .defer(d3.csv,'../data/olympic_medal_count_1900.csv',parse)//d3.csv is 2 arguments
+    .defer((d3.csv,'../data/olympic_medal_count_1960.csv',parse))
+    .defer((d3.csv,'../data/olympic_medal_count_2012.csv',parse))
     .await(function(err,rows1900,rows1960,rows2012){
+      console.log(rows1900);
+      console.log(rows1960);
+      console.log(rows2012);
+
+
         //Draw axis
         scaleY = d3.scaleLinear()
             .domain([0,120])
             .range([h,0]);
         scaleX = d3.scaleLinear()
             .domain([0,4])
-            .range([0,w]);
+            .range([0,w]); //spaces them equally along the width of the screen
 
         var axisY = d3.axisLeft()
-            .scale(scaleY)
+            .scale(scaleY) //left side of screen. ticks & numbers go to left by default.
+            //negative tick size reverses tick (right). ticks line entire width of chart.
             .tickSize(-w-200);
 
         plot.append('g')
@@ -40,19 +47,56 @@ d3.queue()
             .call(axisY);
 
         //Step 2: implement the code to switch between three datasets
-        d3.select('#year-1900').on('click', function(){
-            draw(rows1900);
+        d3.select('#year-1900').on('click', function(){ //refers to 1900 button. selets button, listens for click events
+            draw(rows1900); //code needs to be re-usable.
+        }); //draws 1900 when button is clicked.
+
+        d3.select('#year-1960').on('click', function(){
+            draw(rows1960);
         });
 
+        d3.select('#year-2012').on('click', function(){
+            draw(rows2012);
+        });
     });
 
+
 //Step 3: implement the enter / exit / update pattern
-function draw(rows){
+function draw(rows){ //represents top 5 countries in given row
     var top5 = rows.sort(function(a,b){
-        return b.count2012 - a.count2012;
-    }).slice(0,5);
+        return b.count - a.count;
+    }).slice(0,5); //gives you 5 elements
 
     console.log(top5);
+    //Update set
+    plot.selectAll('country')
+      .data(top5, function(d){ return d.country})//second arguement is the matching criteria (It is the key). One element consistantly representing the same thing.
+    //Enter set
+    var enter = update.enter()
+      .append('g').attr('class','country')
+      .attr('transform',function(d,i){//d stands for data, i stands for index. index is order of the element
+        return 'translate(' + scaleX(i) + '.0)';
+      });
+    enter.append('rect')
+      .attr('x',0)//doesn't matter
+      .attr('y', function(d){//needs to be set by data (d).
+        return scaleY(d.count);//60 into scale y gives half height (half of 120).
+      })
+      .attr('width',8)//doesn't matter
+      .attr('height', function(d){//needs to be set by data
+        return h - scaleY(d.count); //h over 2. a count of 90 would be 3/4 the height, 1/4 from the top.
+      })
+    enter.append('text')
+      .attr('y',function(d){ return scaleY(d.count)})
+      .text(function(d){return d.country})
+      .attr('text-anchor','middle');
+
+    //exit
+    var exit = update.exit().remove();
+
+    //update
+    var update = update.update().exit();
+
 }
 
 function parse(d){
